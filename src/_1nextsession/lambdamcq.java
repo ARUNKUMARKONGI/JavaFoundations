1e8a779c5401126076e365dc2a9d32468aa62c1b
ARUNKUMARKONGI <arunkongi@gmail.com>
2023-12-22 14:55:15 +0530

    ok

--- src/_1nextsession/lambdamcq.java
+++ src/_1nextsession/lambdamcq.java
@@ -41,8 +41,28 @@ class lambdamcq {
         players.add(new CricketPlayer("Steve Smith", 8500, 27));
         players.add(new CricketPlayer("Kane Williamson", 8000, 23));
         players.add(new CricketPlayer("Joe Root", 8500, 20));
+        /*for (int i = 0; i < 5; i++) {
+            System.out.print("Enter player name: ");
+            String name = scanner.next();
+            System.out.print("Enter runs: ");
+            int runs = scanner.nextInt();
+            System.out.print("Enter centuries: ");
+            int centuries = scanner.nextInt();
 
+            players.add(new CricketPlayer(name, runs, centuries));
+        }*/
+
+        for (CricketPlayer i : players) {
+            System.out.println(i.getName() + " - Centuries: " + i.getCenturies());
+        }
+
+        for (CricketPlayer player : players) {
+            if (player.getCenturies() > 0) {
+                System.out.println(player);
+            }
+        }
         // Using Lambda Expression to filter players with more than 8000 runs
         players.stream().filter(player -> player.getRuns() > 8000).forEach(System.out::println);
+       // players.stream().filter(player -> player.getCenturies() > 0).forEach(System.out::println);
     }
 }
